db13c55f4446965ac8bd37fb6527968a9eba6def
akane_hikita <akane.hikita.cd@ambl.co.jp>
2022-11-14 18:45:06 +0900

    update 全項目に独自のerrorMapを設定

--- src/components/models/UserFormSchema.ts
+++ src/components/models/UserFormSchema.ts
@@ -5,28 +5,53 @@ import { GENDER } from '@/components/enums/Gender';
  * カスタムエラーメッセージ
  * node_modules\zod\lib\locales\en.js参考に
  */
-const customErrorMap: zod.ZodErrorMap = (issue, ctx) => {
-  // true/falseチェックボックス用メッセージ
-  if (issue.code === zod.ZodIssueCode.invalid_literal) {
-    return { message: 'チェック必須です' };
+const customErrorMap: zod.ZodErrorMap = (error, ctx) => {
+  switch (error.code) {
+    case zod.ZodIssueCode.invalid_type:
+      if (error.received === zod.ZodParsedType.undefined) {
+        return { message: '必須項目です' };
+      } else {
+        return { message: '値に誤りがあります' };
+      }
+
+    case zod.ZodIssueCode.invalid_enum_value:
+      return { message: '値に誤りがあります' };
+
+    case zod.ZodIssueCode.invalid_literal:
+      // true/falseチェックボックス用メッセージ
+      return { message: 'チェック必須です' };
+
+    case zod.ZodIssueCode.too_big:
+      return { message: `${error.inclusive}文字以内で入力してください` };
+
+    case zod.ZodIssueCode.too_small:
+      if (error.type === 'array') {
+        return { message: `${error.minimum}つ以上チェックしてください` };
+      }
+      return { message: `${error.minimum}文字以上で入力してください` };
+
+    case zod.ZodIssueCode.invalid_string:
+      if (error.validation === 'email') {
+        return { message: 'メールアドレスの形式で入力してください' };
+      }
+      if (error.validation === 'regex') {
+        return { message: '半角英数字記号（_-）で入力してください。記号は先頭の配置不可です。' };
+      }
   }
+
+  // デフォルトのメッセージを返す
+  return { message: ctx.defaultError };
 };
 
 export const UserFormSchema = zod.object({
-  userName: zod.string().max(15, { message: '15文字以内で入力してください' }).optional(),
+  userName: zod.string({ errorMap: customErrorMap }).max(15).optional(),
   userID: zod
-    .string({ required_error: '必須項目です' })
-    .regex(/^[a-zA-Z0-9]+[a-zA-Z0-9_-]+$/, {
-      message: '半角英数字記号（_-）で入力してください。記号は先頭の配置不可です。',
-    })
-    .min(5, { message: '5文字以上で入力してください' })
-    .max(15, { message: '15文字以内で入力してください' }),
-  gender: zod.nativeEnum(GENDER, {
-    required_error: '必須項目です',
-    invalid_type_error: '選択値に誤りがあります',
-  }),
-  // TODO 初回に未選択でsubmitした場合、送られるのがundefinedのためinvalid_typeエラーとなり、メッセージが「Required」と出力されてしまう
-  interest: zod.string().array().min(1, { message: '必須項目です。' }),
-  email: zod.string({ required_error: '必須項目です' }).email({ message: 'メールアドレスの形式で入力してください' }),
+    .string({ errorMap: customErrorMap })
+    .regex(/^[a-zA-Z0-9]+[a-zA-Z0-9_-]+$/)
+    .min(5)
+    .max(15),
+  gender: zod.nativeEnum(GENDER, { errorMap: customErrorMap }),
+  interest: zod.array(zod.string(), { errorMap: customErrorMap }).min(1),
+  email: zod.string({ errorMap: customErrorMap }).email(),
   confirm: zod.literal(true, { errorMap: customErrorMap }),
 });
