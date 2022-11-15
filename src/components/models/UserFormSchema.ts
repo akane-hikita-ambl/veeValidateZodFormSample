239c13f6ee6ac0de6adeba641a4328c500a5b1fb
akane_hikita <akane.hikita.cd@ambl.co.jp>
2022-11-15 13:21:55 +0900

    update 共通のzodErrorMapとスキーマのzodErrorMapを分離

--- src/components/models/UserFormSchema.ts
+++ src/components/models/UserFormSchema.ts
@@ -1,57 +1,35 @@
 import * as zod from 'zod';
 import { GENDER } from '@/components/enums/Gender';
+import { JapaneseErrorMap } from '@/util/zod/JapaneseErrorMap';
 
 /**
- * カスタムエラーメッセージ
- * node_modules\zod\lib\locales\en.js参考に
+ * UserForm用カスタムエラーメッセージ
  */
-const customErrorMap: zod.ZodErrorMap = (issue, ctx) => {
+const UserFormErrorMap: zod.ZodErrorMap = (issue, ctx) => {
   switch (issue.code) {
-    case zod.ZodIssueCode.invalid_type:
-      if (issue.received === zod.ZodParsedType.undefined) {
-        return { message: '必須項目です' };
-      } else {
-        return { message: '値に誤りがあります' };
-      }
-
-    case zod.ZodIssueCode.invalid_enum_value:
-      return { message: '値に誤りがあります' };
-
     case zod.ZodIssueCode.invalid_literal:
       // true/falseチェックボックス用メッセージ
       return { message: 'チェック必須です' };
 
-    case zod.ZodIssueCode.too_big:
-      return { message: `${issue.inclusive}文字以内で入力してください` };
-
-    case zod.ZodIssueCode.too_small:
-      if (issue.type === 'array') {
-        return { message: `${issue.minimum}つ以上チェックしてください` };
-      }
-      return { message: `${issue.minimum}文字以上で入力してください` };
-
     case zod.ZodIssueCode.invalid_string:
-      if (issue.validation === 'email') {
-        return { message: 'メールアドレスの形式で入力してください' };
-      }
       if (issue.validation === 'regex') {
         return { message: '半角英数字記号（_-）で入力してください。記号は先頭の配置不可です。' };
       }
   }
 
   // デフォルトのメッセージを返す
-  return { message: ctx.defaultError };
+  return { message: JapaneseErrorMap(issue, ctx).message };
 };
 
 export const UserFormSchema = zod.object({
-  userName: zod.string({ errorMap: customErrorMap }).max(15).optional(),
+  userName: zod.string().max(15).optional(),
   userID: zod
-    .string({ errorMap: customErrorMap })
+    .string({ errorMap: UserFormErrorMap })
     .regex(/^[a-zA-Z0-9]+[a-zA-Z0-9_-]+$/)
     .min(5)
     .max(15),
-  gender: zod.nativeEnum(GENDER, { errorMap: customErrorMap }),
-  interest: zod.array(zod.string(), { errorMap: customErrorMap }).min(1),
-  email: zod.string({ errorMap: customErrorMap }).email(),
-  confirm: zod.literal(true, { errorMap: customErrorMap }),
+  gender: zod.nativeEnum(GENDER),
+  interest: zod.array(zod.string()).min(1),
+  email: zod.string().email(),
+  confirm: zod.literal(true, { errorMap: UserFormErrorMap }),
 });
